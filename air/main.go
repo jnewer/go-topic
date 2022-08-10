788218b5f65e19474d5cd512953a9df54a2875ee
jcoder <jeekang@qq.com>
2022-08-10 20:54:22 +0800

    add gin-gorm-page

---
+++ air/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "go air",
+		})
+	})
+
+	r.Run()
+}
